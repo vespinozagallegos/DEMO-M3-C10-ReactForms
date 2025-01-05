3d837b058e7ce3bfd73f3c964d52f827faa7798e
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 19:40:42 -0300

    Se agregaron los ejemplos de la lecture

---
+++ Clase 10 React Forms/src/EjemplosTeóricos/EjConJSX/EstructuraBase.jsx
@@ -0,0 +1,21 @@
+function Form() {
+  return (
+    <form>
+      <h2>LOGIN</h2>
+
+      <div>
+        <label>USERNAME</label>
+        <input />
+      </div>
+      
+      <div>
+        <label>PASSWORD</label>
+        <input />
+      </div>
+
+      <button>SUBMIT</button>
+    </form>
+  );
+}
+
+// export default Form;
