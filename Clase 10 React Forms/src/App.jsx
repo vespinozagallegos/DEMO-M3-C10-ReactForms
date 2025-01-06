5654af0e4145058c9dbb6827544dd365ed0fd370
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-06 16:04:33 -0300

    Se corrigieron comentarios

--- Clase 10 React Forms/src/App.jsx
+++ Clase 10 React Forms/src/App.jsx
@@ -3,23 +3,34 @@ import { useState } from 'react';
 
 function App() {
   // Guarda info que el usuario va cargando en los inputs
-  // Para eso el estado es un obj con 2 propiedades
+  // Para eso el estado es un obj con 2 propiedades username y password
+  
+  // *Al trabajar con estados controlados se debe enlazar el estado de React con el valor de los inputs 
+  // *Porque EL ESTADO debe ser IGUAL al value del input (username y password)
+  // Esta validación con JS (propiedades del estado username y password, iguales al value de los inputs)
+  // es la correcta y no con HTML --> required
   const [userData, setUserData] = useState({
     username: "",
     password: ""
   });
   console.log(userData);
+
+
   
-  // Se agrega funcionalidad a los inputs(sin esto no se puede escribir en ellos)
+  // Esta funcion agrega funcionalidad a los inputs(sin esto no se puede escribir en ellos) SE BINDEA con onChange={handleInputChange}
   // Cada caracter que se ingrese, actualizara el valor del input y se debe guardar
-  //AL BINDEAR el input se recibe un objeto EVENT que se dispara ante cada cambio en el input
+  // !AL BINDEAR el input se recibe un objeto EVENT que se dispara ante cada cambio en el input 
+  // Aqui se ve onChange={(event) => handleInputChange(event)}) pero ocuparemos la mas simple onChange={handleInputChange}
   const handleInputChange = (event) => {
+    // para ver que las propiedades estan en target
+    console.log(event);
+
+    // destructuring del objeto event.target.
+    const {name, value} = event.target;
+    // actualizar el estado
+    setUserData({})
 
   }
-  
-  // *Al trabajar con estados controlados se debe enlazar el estado de React con el valor de los inputs 
-  // !Porque EL ESTADO debe ser IGUAL al value del input (USERNAME Y PASSWORD)
-  // Esta validación con JS (ESTADO) es la correcta y no con HTML --> required
   
   return (
     <form className={styles.container}>
@@ -30,7 +41,7 @@ function App() {
         <label>Username: </label>
         <input 
         type='text'
-        // al poner value se BINDEA(enlaza) el ESTADO con el INPUT
+        // !al poner value se BINDEA(enlaza) el ESTADO con el INPUT
         value={userData.username}
         // El name de input nos indicará DONDE estará ESCRIBIENDO EL usuario
         name='username'
@@ -50,7 +61,7 @@ function App() {
         value={userData.password}
         name='password'
         placeholder='**********'
-        // Bindeo de la funcion con el input
+        // !Bindeo de la funcion con el input
         onChange={handleInputChange}
         />
       </div>
--- Clase 10 React Forms/src/EjemplosTeóricos/1AppEstructuraBasicaDeFormularioControlado.jsx
+++ Clase 10 React Forms/src/EjemplosTeóricos/1AppEstructuraBasicaDeFormularioControlado.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react';
 function App() {
   // Guarda info que el usuario va cargando en los inputs
   // Para eso el estado es un obj con 2 propiedades
+  
+  // *Al trabajar con estados controlados se debe enlazar el estado de React con el valor de los inputs 
+  // *Porque EL ESTADO debe ser IGUAL al value del input (username y password)
+  // Esta validación con JS (propiedades del estado username y password, iguales al value de los inputs)
+  // es la correcta y no con HTML --> required
   const [userData, setUserData] = useState({
     username: "",
     password: ""
   });
   
-  // *Al trabajar con estados controlados se debe enlazar el estado de React con el valor de los inputs 
-  // !Porque EL ESTADO debe ser IGUAL al value del input (USERNAME Y PASSWORD)
-  // Esta validación con JS (ESTADO) es la correcta y no con HTML --> required
   
   return (
     <form className={styles.container}>
@@ -22,7 +24,7 @@ function App() {
         <label>Username: </label>
         <input 
         type='text'
-        // al poner value se BINDEA(enlaza) el ESTADO con el INPUT
+        // !al poner value se BINDEA(enlaza) el ESTADO con el INPUT
         value={userData.username}
         // El name de input nos indicará DONDE estará ESCRIBIENDO EL usuario
         name='username'
